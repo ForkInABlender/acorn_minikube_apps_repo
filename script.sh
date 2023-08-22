35d2e82837dffec48a6516e5808f49bc384b4688
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-08-22 12:37:39 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,16 @@
+#Dylan Kenneth Eliot
+#
+#Configure minikube for custom runtime
 minikube start --container-runtime=containerd
-sleep .1
+#Return if it times out on enabling...
 minikube addons enable ingress
-sleep 3
+#same for acorn.....
 acorn install --ingress-class-name nginx
+#build & run the app
+#
+#Make sure that you run  'minikube tunnel' before you build and run in a separate terminal window. This will allow the ingress controller to assign
+# a domain name with an ip...
+#
 acorn build .
 acorn run .
 acorn apps
