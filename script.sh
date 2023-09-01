c7172aae8906ad0547bcb8754919bf12967e326f
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-09-01 18:18:36 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,38 +1,46 @@
 #Dylan Kenneth Eliot
 #
-minikube start --container-runtime=docker --driver=docker
-minikube addons enable ingress
-acorn install --ingress-class-name nginx
 echo """
+https://chat.openai.com/share/f61405ef-a70c-4e4a-b8c3-e81b0ed1767d
+
+Refer to the above link for debugging purposes. Even for running in docker rootless mode.
+This helped with figuring out how to stand it up locally. One way in minikube and one way in kind.
+
+Use with ngrok, selenium webdriver, a install of google-chrome, and credentials needed to login to
+ cloudflare. You can practically automate the process. With tmux, it will run in the background.
+  The URLs are temporary with ngrok.
+"""
+
+
+echo """
+# kind-config.yaml
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
-- role: worker
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+  - containerPort: 5000
+    hostPort: 5000
+    protocol: TCP
+  - containerPort: 8080
+    hostPort: 8080
+    protocol: TCP
+
 """ > kind-config.yaml
 kind create cluster cluster --config kind-config.yaml
-kubectl config use-context minikube
-kubectl get pods --all-namespaces -o yaml > pods.yaml
-kubectl get namespaces --all-namespaces -o yaml > namespaces.yaml
-kubectl get deployments --all-namespaces -o yaml > deployments.yaml
-kubectl get secrets --all-namespaces -o yaml > secrets.yaml
-kubectl get pods --all-namespaces -o json | jq '.items[].metadata.annotations' > pod_annotations.json
-kubectl get services --all-namespaces -o yaml > services.yaml
-kubectl get configmaps --all-namespaces -o yaml > configmaps.yaml
-
-kubectl config use-context kind-kind
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -f namespaces.yaml
-kubectl apply -f services.yaml
-kubectl apply -f deployments.yaml
-kubectl apply -f pods.yaml
-cat pod_annotations.json | jq -r 'to_entries[] | "\(.key) \(.value | to_entries[] | "\(.key)=\(.value)")"' | while read pod annotations; do
-    kubectl annotate pods $pod $annotations --overwrite
-done
-kubectl apply -f configmaps.yaml
 acorn install --ingress-class-name nginx
-minikube delete
 acorn build .
 acorn run .
 acorn apps
