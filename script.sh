6dba9a667d2edcfc7b2195e14d821d77cc348b4e
ForkInABlender <92487561+ForkInABlender@users.noreply.github.com>
2023-10-12 17:48:15 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,6 +10,7 @@ This targets traefik instead of the standard nginx-ingress controller.
 k3d cluster create # do this in a seperate tab from below unless the line returns a normal response
 # waiting to run the next command. ..... tmux may also help for this as a temporary fix...
 
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 curl -sfL https://get.k3s.io | bash
 mkdir ~/.kube -p
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config && sudo chown $USER ~/.kube/config
